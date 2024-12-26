0524d21d9662331d10488c7de1427379424de189
VUHUQ25133 <146147897+VUHUQ25133@users.noreply.github.com>
2024-12-26 10:39:30 +0700

    Update PYKT2048 - ỨNG DỤNG STACK TRONG PYTHON.py

--- PYKT2048 - ỨNG DỤNG STACK TRONG PYTHON.py
+++ PYKT2048 - ỨNG DỤNG STACK TRONG PYTHON.py
@@ -1,10 +1,40 @@
-from sys import stdin, setrecursionlimit
+# Biểu thức ngoặc là xâu chỉ gồm các ký tự ‘(’ hoặc ‘)’. 
+# Biểu thức ngoặc đúng và bậc của biểu thức ngoặc được định nghĩa một cách đệ qui như sau:
+#    Biểu thức rỗng là biểu thức ngoặc đúng và có bậc bằng 0,
+#    Nếu A là biểu thức ngoặc đúng có bậc bằng k thì (A) cũng là một biểu thức ngoặc đúng có bậc bằng k+1,
+#    Nếu A và B là hai biểu thức ngoặc đúng và có bậc tương ứng là k_1 và k_2 
+#       thì AB cũng là một biểu thức ngoặc đúng có bậc bằng max(k_1,k_2).
+# Ví dụ, ‘()(())’ là một biểu thức ngoặc đúng có bậc bằng 2 còn ‘(()(()))’ là một biểu thức ngoặc đúng và có bậc bằng 3.
 
+# Cho số nguyên K và xâu S là một xâu chỉ gồm các ký tự ‘(‘, ‘)’ và ‘?’, 
+# hãy đếm số cách cách thay các ký tự ‘?’ trong xâu S thành ký tự ‘(‘ hoặc ‘)’ 
+#    để nhận được xâu T là biểu thức ngoặc đúng có bậc bằng K.
+
+# Input
+#    Dòng đầu chứa số nguyên dương K.
+#    Dòng thứ hai chứa xâu S chỉ gồm các ký tự ‘(‘, ‘)’ và ‘?’.
+#     	Giới hạn:
+#	    Subtask 1 (50%) độ dài xâu S không vượt quá 20.
+#	    Subtask 2 (50%) độ dài xâu S không vượt quá 200.
+
+# Output
+#    Hãy in ra đáp án trên một dòng.
+
+# Input		Output
+# 2
+# ????(?		1
+
+# 1
+# ((???(		0
+
+# 2
+#((???()???(?	4
+
+from sys import stdin, setrecursionlimit
 
 test = list(stdin.read().split())
 
 setrecursionlimit(10000000)
-
 
 def solve(i, cnt, d):
     if cnt > k: return 0
@@ -29,5 +59,5 @@ def solve(i, cnt, d):
 k = int(test[0])
 s = test[1]
 n = len(s)
-dp = [[[-1] * (n+1) for j in range(k+1)] for i in range(n+1)]
+dp = [[[-1] * (n+1) for j in range(k + 1)] for i in range(n + 1)]
 print(solve(0, 0, 0))
